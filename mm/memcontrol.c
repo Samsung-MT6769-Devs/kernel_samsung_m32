10eb5f50eb381a1b867fedb4b8e744dc7003bf88
Zears14 <79321017+Zears14@users.noreply.github.com>
2025-06-23 21:35:26 +0700

    cgroup: improve resource cleanup in css_free_work_fn and __mem_cgroup_free
    Signed-off-by: Zears14 <79321017+Zears14@users.noreply.github.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4361,9 +4361,7 @@ static void __mem_cgroup_free(struct mem_cgroup *memcg)
 
 	for_each_node(node)
 		free_mem_cgroup_per_node_info(memcg, node);
-	free_percpu(memcg->stat);
-	/* poison memcg before freeing it */
-	memset(memcg, 0x78, sizeof(struct mem_cgroup));
+	free_percpu(memcg->stat_cpu);
 	kfree(memcg);
 }
 
